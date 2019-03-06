ab493ace5722d7156fbcb9927f3677e4756aae2a
meghats123 <meghats39032@gmail.com>
2019-03-06 12:45:00 +0530

    Admin page completed

--- app/src/main/java/com/mca/administrator/contactapp/AdminLogin.java
+++ app/src/main/java/com/mca/administrator/contactapp/AdminLogin.java
@@ -1,13 +1,39 @@
 package com.mca.administrator.contactapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class AdminLogin extends AppCompatActivity {
-
+EditText ed1,ed2;
+Button b;
+String s1,s2;
+String username="admin",pswd="admin";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_login);
+        ed1=(EditText)findViewById(R.id.un);
+        ed2=(EditText)findViewById(R.id.password);
+        b=(Button)findViewById(R.id.adlogin);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                s1=ed1.getText().toString();
+                s2=ed2.getText().toString();
+                if ((s1.equals(username))&&(s2.equals(pswd))){
+                    Intent i = new Intent(getApplicationContext(),AdminPage.class);
+                    startActivity(i);
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "You are not Authorized",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/mca/administrator/contactapp/AdminPage.java
@@ -0,0 +1,29 @@
+package com.mca.administrator.contactapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class AdminPage extends AppCompatActivity {
+Button b1,b2,b3;
+String s1,s2,s3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_page);
+        b1=(Button)findViewById(R.id.ac);
+        b2=(Button)findViewById(R.id.uc);
+        b3=(Button)findViewById(R.id.dc);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),Registration.class);
+                startActivity(i);
+            }
+        });
+
+    }
+}
